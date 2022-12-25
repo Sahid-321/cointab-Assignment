8a19d508ab42ba7dccfb2821dd353f07b0ca2812
sahid jamal <sahidjamal151@gmail.com>
2022-12-25 15:37:54 +0530

    project completed

--- coinTab-Assignment/frontend/src/components/Page1.jsx
+++ coinTab-Assignment/frontend/src/components/Page1.jsx
@@ -1,22 +1,28 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import axios from 'axios'
+import { Button } from "react-bootstrap";
 const PageOne = () => {
     const navigate = useNavigate();
-   
+   const [count, setCount] = useState(0)
   
     const handleFetch = () => {
+setCount(count+1);
+if(count ==1){
 alert("User Data Fetched and Added in your MongoDB")
    axios.post('http://localhost:8080/post')
             .then((res)=> console.log('User Data Fetched and Added in your MongoDB', res))
             .catch((err)=> console.log('Something went wrong', err))
             
        
-    
+}else{
+    alert("Data alredy added")
+}
 // console.log(userValue);
     }
 
     const handleDelete = () => {
+        setCount(1);
         alert("User data deleted from MongoDB");
         axios.delete('http://localhost:8080/delete')
         .then((res)=> console.log('User Data delete in your MongoDB', res))
@@ -30,9 +36,9 @@ alert("User Data Fetched and Added in your MongoDB")
     return (
         <>
             <h1>Home Page</h1>
-            <button onClick={handleFetch}>Fetch User</button>
-            <button onClick={handleDelete}>Delete User</button>
-            <button onClick={handleDetails}>User Details</button>
+            <Button onClick={handleFetch}>Fetch User</Button>
+            <Button onClick={handleDelete}>Delete User</Button>
+            <Button onClick={handleDetails}>User Details</Button>
         </>
     )
 }
