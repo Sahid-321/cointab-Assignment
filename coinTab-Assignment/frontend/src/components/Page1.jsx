30b039c39184a2f3945f297023312b29b449ab74
sahid jamal <sahidjamal151@gmail.com>
2022-12-25 11:51:43 +0530

    data stored and deleted in mongodb

--- coinTab-Assignment/frontend/src/components/Page1.jsx
+++ coinTab-Assignment/frontend/src/components/Page1.jsx
@@ -1,58 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom"
 import axios from 'axios'
 const PageOne = () => {
     const navigate = useNavigate();
     const [storeUser, setStoreUser] = useState([])
-    const [userValue, setUserValue] = useState({
-        title: "",
-        first: "",
-        last:"",
-        gender:"",
-        email:"",
-        country:"",
-        img:"",
-        age:""
-    })
-
-    //fetch data
-    function getUsers() {
-        fetch("https://randomuser.me/api/?results=50")
-            .then((results) => {
-                return results.json();
-            })
-          
-            .then((data) => {
-                
-                setStoreUser(data.results);
-                // Access your data here
-            });
-    }
-
-storeUser.map((elem)=>{
-   setUserValue({
-    title: elem.name.title,
-        first: elem.name.first,
-        last:elem.name.last,
-        gender:elem.gender,
-        email:elem.email,
-        country:elem.location.country,
-        img: elem.picture.large,
-        age: elem.dob.age
-   })
-})
-console.log(userValue);
+    const [userValue, setUserValue] = useState([])
+  
     const handleFetch = () => {
-        getUsers()
+alert("User Data Fetched and Added in your MongoDB")
+   axios.post('http://localhost:8080/post')
+            .then((res)=> console.log('User Data Fetched and Added in your MongoDB', res))
+            .catch((err)=> console.log('Something went wrong', err))
+            
        
-// axios.post('http://localhost:8080/post', storeUser)
-// .then((res)=> alert('User Data Fetched and Added in your MongoDB'))
-// .catch((err)=> alert('Something went wrong', err))
-
+    
+// console.log(userValue);
     }
 
     const handleDelete = () => {
-        alert("Deleted")
+        alert("User data deleted from MongoDB");
+        axios.delete('http://localhost:8080/delete')
+        .then((res)=> console.log('User Data delete in your MongoDB', res))
+        .catch((err)=> console.log('Something went wrong', err))
+        
     }
 
     const handkeDetails = () => {
--- coinTab-Assignment/server/index.js
+++ coinTab-Assignment/server/index.js
@@ -1,32 +1,116 @@
-const express = require('express');
-const mongoose = require('mongoose')
-const cors = require('cors')
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors'
 const app = express();
-
 const PORT = 8080;
+import fetch from 'node-fetch';
 
 app.use(express.json())
 app.use(cors())
 
-mongoose.connect("mongodb://localhost:27017/userdetails",()=>{
+var arr = [];
+
+mongoose.connect("mongodb://localhost:27017/details", () => {
     console.log("mongoDB connected");
 })
 
 const mongoSchema = mongoose.Schema({
-    name: String,
-    gender: String
+    title: {
+        type: String,
+
+    },
+    first: {
+        type: String,
+    },
+    last: {
+        type: String,
+
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    country: {
+        type: String,
+    },
+    img: {
+        type: String,
+    },
+    age: {
+        type: String,
+    }
 })
 
-const User = mongoose.model("user", mongoSchema);
+const User = mongoose.model("data", mongoSchema);
 
-app.post('/post', (req,res)=>{
-    User.insertMany({
-        name: "",
-        gender:req.body.gender
-    })
-console.log(req.body.gender);
+function userFnc() {
+
+    fetch(`https://randomuser.me/api/?seed=foobar&results=50`)
+        .then((res) => {
+            return res.json()
+        })
+        .then((data) => {
+            return data.results
+        })
+        .then((data1) => {
+            for (var i = 0; i < data1.length; i++) {
+
+
+                User.insertMany({
+                    title: data1[i].name.title,
+                    first: data1[i].name.first,
+                    last: data1[i].name.last,
+                    gender: data1[i].gender,
+                    email: data1[i].email,
+                    country: data1[i].location.country,
+                    img: data1[i].picture.large,
+                    age: data1[i].dob.age
+                })
+
+
+                // User.insertMany(arr)
+                //    User.deleteMany(arr)
+
+            }
+
+        })
+        .catch((err) => console.log("error"))
+
+
+}
+
+
+app.post('/post', async (req, res) => {
+    console.log("hello");
+   try {
+   await userFnc()
+   } catch (error) {
+    console.log(error);
+   }
+
 })
 
-app.listen(PORT,()=>{
+
+app.delete('/delete', (req, res) => {
+    console.log("delete");
+    User.find()
+        .then((item) => {
+            for (var i = 0; i < item.length; i++) {
+
+                User.findByIdAndDelete({
+                    _id: item[i]._id
+                })
+                    .then((doc) => console.log(doc))
+                    .catch((err) => console.log(err))
+
+            }
+        }
+        )
+
+})
+
+
+
+app.listen(PORT, () => {
     console.log(`server running at port ${PORT}`);
 })
